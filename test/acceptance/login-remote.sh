6a86bcf77f77390d671eac5ea5c4096d8272f2f6
Andrew Martin <sublimino@gmail.com>
2016-06-01 18:13:01 +0100

    fix: clear env in tests

--- test/acceptance/login-remote.sh
+++ test/acceptance/login-remote.sh
@@ -3,12 +3,14 @@
 load test_helper
 
 @test "login: accepts --registry-pass arg" {
-    run_refute  ${APP} login --registry-user=123 --registry-pass=123
+    run_refute env -i \
+      ${APP} login --registry-user=123 --registry-pass=123
     assert_output_contains "unauthorized: incorrect username or password"
 }
 
 @test "login: succeeds with valid credentials" {
-    run_assert  ${APP} login \
+    run_assert env -i \
+      ${APP} login \
       --registry-host=registry.binarysludge.com \
       --registry-user=test-rollcage-user \
       --registry-pass='&B518isz0yaX!GYa$c2fnF'
@@ -16,34 +18,38 @@ load test_helper
 }
 
 @test "login: accepts environment variable for user" {
-    REGISTRY_USER='test-rollcage-user' \
-      run_assert  ${APP} login \
+    run_assert env -i \
+      REGISTRY_USER='test-rollcage-user' \
+      ${APP} login \
       --registry-host=registry.binarysludge.com \
       --registry-pass='&B518isz0yaX!GYa$c2fnF'
     assert_output_contains "Login Succeeded"
 }
 
 @test "login: accepts environment variable for password" {
-    REGISTRY_PASS='&B518isz0yaX!GYa$c2fnF' \
-      run_assert  ${APP} login \
+    run_assert env -i \
+      REGISTRY_PASS='&B518isz0yaX!GYa$c2fnF' \
+      ${APP} login \
       --registry-host=registry.binarysludge.com \
       --registry-user=test-rollcage-user
     assert_output_contains "Login Succeeded"
 }
 
 @test "login: accepts environment variable for host" {
-    REGISTRY_HOST='registry.binarysludge.com' \
-      run_assert  ${APP} login \
+    run_assert env -i \
+      REGISTRY_HOST='registry.binarysludge.com' \
+      ${APP} login \
       --registry-user=test-rollcage-user \
       --registry-pass='&B518isz0yaX!GYa$c2fnF'
     assert_output_contains "Login Succeeded"
 }
 
 @test "login: prioritises arguments over environment variables" {
-    REGISTRY_USER='wrong-user' \
-    REGISTRY_PASS='wrong-pass' \
-    REGISTRY_HOST='wrong-registry.binarysludge.com' \
-      run_assert  ${APP} login \
+    run_assert env -i \
+      REGISTRY_USER='wrong-user' \
+      REGISTRY_PASS='wrong-pass' \
+      REGISTRY_HOST='wrong-registry.binarysludge.com' \
+      ${APP} login \
       --registry-host=registry.binarysludge.com \
       --registry-user=test-rollcage-user \
       --registry-pass='&B518isz0yaX!GYa$c2fnF'
