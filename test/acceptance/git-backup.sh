a6f017ba00930f7e2e900dfd4a52f38141838488
Andrew Martin <sublimino@gmail.com>
2016-06-14 09:50:50 +0100

    fix: run git-backup in-process

--- test/acceptance/git-backup.sh
+++ test/acceptance/git-backup.sh
@@ -4,49 +4,57 @@ load test_helper
 
 @test "git-backup: accepts action" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=x
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=x
     refute_output_contains "Action required"
 }
 
 @test "git-backup: accepts --repo-user" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=horatio
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=horatio
     assert_output_contains "github.com/horatio/rollcage.git"
 }
 
 @test "git-backup: accepts --git-host" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=nelson --git-host=example.com
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=nelson --git-host=example.com
     assert_output_contains "example.com/nelson/rollcage.git"
 }
 
 @test "git-backup: outputs string with git commands" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=x
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=x
     assert_output_contains "git push --verbose --atomic --prune"
 }
 
 @test "git-backup: outputs string with user env vars" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=x
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=x
     assert_output_contains "\${GIT_BACKUP_USER}"
 }
 
 @test "git-backup: outputs string with pass env vars" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=x
-    assert_output_contains "\${GIT_BACKUP_PASS}"
+      GIT_BACKUP_PASS=this-is-a-backup-pass \
+      ${APP} git-backup --dry-run --git-owner=x
+    assert_output_contains "this-is-a-backup-pass"
 }
 
 @test "git-backup: checks out \$CI_BUILD_REF_NAME when present" {
     run_assert env -i \
+      GIT_BACKUP_PASS=test \
       CI_BUILD_REF_NAME=the-ultimate-branch \
-      ${APP} git-backup --git-owner=x
+      ${APP} git-backup --dry-run --git-owner=x
     assert_output_contains "git checkout the-ultimate-branch"
 }
 
 @test "git-backup: does not check out \$CI_BUILD_REF_NAME when missing" {
     run_assert env -i \
-      ${APP} git-backup --git-owner=x
+      GIT_BACKUP_PASS=test \
+      ${APP} git-backup --dry-run --git-owner=x
     refute_output_contains "git checkout"
 }
