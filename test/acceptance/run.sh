4e392d44accb3f43069bf40e65d10d423f718b74
Andrew Martin <sublimino@gmail.com>
2016-06-17 11:52:29 +0100

    feat: add run command

---
+++ test/acceptance/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bats
+
+load test_helper
+
+@test "run: accepts run action" {
+    run_assert ${APP} \
+      run
+}
+
+@test "run: runs command" {
+    run_assert ${APP} \
+      run echo 'testerino'
+    assert_output_contains "testerino"
+}
+
+@test "run: supresses '-it' with --interactive arg" {
+    run_assert ${APP} \
+      run --interactive=false echo 'testerino'
+    assert_output_contains "docker run rollcage:dev echo testerino"
+}
+
+@test "run: passes arguments to docker" {
+    run_assert ${APP} \
+      run -- -v /home:/home -v=/home:/home
+    assert_output_contains "docker run -it -v /home:/home -v=/home:/home rollcage:dev"
+}
+
+@test "run: passes arguments to docker with command" {
+    run_assert ${APP} \
+      run echo 'toast' -- -v /home:/home -v=/home:/home
+    assert_output_contains "docker run -it -v /home:/home -v=/home:/home rollcage:dev echo toast"
+}
