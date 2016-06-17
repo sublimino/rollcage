e7b512cdfe834ad73fbbbc3f7de046e323b1946f
Andrew Martin <sublimino@gmail.com>
2016-06-17 13:11:48 +0100

    tests: fix for CI

--- test/acceptance/run.sh
+++ test/acceptance/run.sh
@@ -9,24 +9,28 @@ load test_helper
 
 @test "run: runs command" {
     run_assert ${APP} \
-      run echo 'testerino'
+      run --dry-run \
+      echo 'testerino'
     assert_output_contains "testerino"
 }
 
 @test "run: supresses '-it' with --interactive arg" {
     run_assert ${APP} \
-      run --interactive=false echo 'testerino'
+      run  --dry-run \
+      --interactive=false echo 'testerino'
     assert_output_contains "docker run rollcage:dev echo testerino"
 }
 
 @test "run: passes arguments to docker" {
     run_assert ${APP} \
+      --dry-run \
       run -- -v /home:/home -v=/home:/home
     assert_output_contains "docker run -it -v /home:/home -v=/home:/home rollcage:dev"
 }
 
 @test "run: passes arguments to docker with command" {
     run_assert ${APP} \
+      --dry-run \
       run echo 'toast' -- -v /home:/home -v=/home:/home
     assert_output_contains "docker run -it -v /home:/home -v=/home:/home rollcage:dev echo toast"
 }
