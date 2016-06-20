6f448ad33f352f652f27f1b283e44a92c377fd9f
Andrew Martin <sublimino@gmail.com>
2016-06-20 12:57:18 +0100

    refactor: prepare 1.0.0-beta

--- test/acceptance/cli.sh
+++ test/acceptance/cli.sh
@@ -8,9 +8,12 @@ load test_helper
     assert_output_contains 'Print version'
 }
 
-@test "cli: has --version" {
-    run ${APP} --version
-    assert_output_contains '0.0.1'
+@test "cli: has semver --version" {
+    run_assert ${APP} --version
+    local SEMVER=( ${output//./ } )
+    assert [ "${SEMVER[0]}" -ge 0 ]
+    assert [ "${SEMVER[1]}" -ge 0 ]
+    assert [ "${SEMVER[2]//\-*/}" -ge 0 ]
 }
 
 @test "parser: rejects empty --pull arg" {
@@ -20,7 +23,6 @@ load test_helper
 @test "parser: rejects invalid --pull arg" {
     run_refute ${APP} --pull --version
 }
-
 
 @test "cli: does not error on empty args" {
     # remove any arguments in the ${APP} variable
