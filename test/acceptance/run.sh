64533f0e60e325f9e4c3bc3f8797a96f9ee19a6d
Andrew Martin <sublimino@gmail.com>
2016-09-09 16:51:09 +0100

    feat: extract tag from image name if present

--- test/acceptance/run.sh
+++ test/acceptance/run.sh
@@ -38,3 +38,23 @@ load test_helper
       run echo 'toast' -- -v /home:/home -v=/home:/home
     assert_output_contains "docker run -it -v /home:/home -v=/home:/home rollcage:dev echo toast"
 }
+
+
+@test "run: parses tag from image" {
+    run_assert ${APP} \
+      --dry-run \
+      --image errordeveloper/kube-installer:latest \
+      run 
+    refute_output_contains "docker run -it errordeveloper/kube-installer:latest:dev"
+    assert_output_contains "docker run -it errordeveloper/kube-installer:latest"
+}
+
+@test "run: overrides parsed tag from image with --tag" {
+    run_assert ${APP} \
+      --dry-run \
+      --image errordeveloper/kube-installer:latest \
+      --tag toast-assault \
+      run 
+    refute_output_contains "docker run -it errordeveloper/kube-installer:toast-assault:dev"
+    assert_output_contains "docker run -it errordeveloper/kube-installer:toast-assault"
+}
